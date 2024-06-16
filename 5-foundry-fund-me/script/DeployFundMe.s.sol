d45fbe782b116cbba4e26c2338ca5a9d04f5ceb9
xzvw <128382145+xzvw@users.noreply.github.com>
2024-06-17 03:33:59 +0800

    Update `DeployFundMe.s.sol` to make the script chain agnostic

--- 5-foundry-fund-me/script/DeployFundMe.s.sol
+++ 5-foundry-fund-me/script/DeployFundMe.s.sol
@@ -2,15 +2,21 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // Before `startBroadcast()`: not a real transaction (will be in the simulated environment)
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
+        // After `startBroadcast()`: a real transaction
 
         // Sepolia ETH / USD address:
         // https://docs.chain.link/data-feeds/price-feeds/addresses
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
 
         vm.stopBroadcast();
 
---
+++ 5-foundry-fund-me/script/HelperConfig.s.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address priceFeed; // ETH / USD price feed addess
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    // If we are on a local anvil chain, we deploy the mock contract
+    // Otherwise, grab the existing contract address from the live network
+    constructor() {
+        if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // ETH / USD address:
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+
+        return mainnetConfig;
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // Sepolia ETH / USD address:
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return sepoliaConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+
+        return anvilConfig;
+    }
+}
