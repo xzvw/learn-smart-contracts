dd95d53557c30bdb41a42eed3eeafe347b9b0f3a
xzvw <128382145+xzvw@users.noreply.github.com>
2024-06-16 14:33:56 +0800

    Add files for `4-foundry-simple-storage`

---
+++ 4-foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    // The main function which is going to be called
+    function run() external returns (SimpleStorage) {
+        // vm:
+        // - Defined in `forge-std`
+        // - Can only be used within Foundry
+        // - Related to something called Foundry cheat codes (?)
+        // - `vm.startBroadcast()` means that everything after this line should be sent to the RPC
+        //   when using the command with `--broadcast` parameter
+        vm.startBroadcast();
+
+        SimpleStorage simpleStorage = new SimpleStorage();
+
+        vm.stopBroadcast();
+
+        return simpleStorage;
+    }
+}
