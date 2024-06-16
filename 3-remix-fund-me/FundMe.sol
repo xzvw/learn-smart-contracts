979089478ad35808f2669ad500aa8605af6fcfde
xzvw <128382145+xzvw@users.noreply.github.com>
2024-06-16 18:08:25 +0800

    Add `getVersion` function to the FundMe contracts

--- 3-remix-fund-me/FundMe.sol
+++ 3-remix-fund-me/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error FundMe__NotOwner();
@@ -78,6 +79,14 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+
+        return priceFeed.version();
     }
 
     receive() external payable {
