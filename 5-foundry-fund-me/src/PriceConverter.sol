dee4dcbf253e159437432eaf9d41c85999ce2783
xzvw <128382145+xzvw@users.noreply.github.com>
2024-06-16 16:49:52 +0800

    Put initial contracts in `5-foundry-found-me`

---
+++ 5-foundry-fund-me/src/PriceConverter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    // To get 1 ETH = ? USD
+    function getPrice() internal view returns (uint256) {
+        // Sepolia ETH / USD address:
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+
+        // 回傳的 answer 小數位數為 8 digits
+        // 我們將它乘上 1e10, 使其小數位數為 18 digits, 與 wei 對齊
+        return uint256(answer) * 1e10;
+    }
+
+    // Input: ETH amount (wei)
+    // Output: ETH amount (wei) in USD
+    function getConversionRate(
+        uint256 ethAmount
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
+    }
+}
